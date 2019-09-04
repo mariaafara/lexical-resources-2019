dca76dca9ccab1a87824d749a37f3fe064ca6762
Timothee Mickus <tmickus@justice.atilf.fr>
2019-09-04 10:16:03 +0200

    small cleanup

--- lexres/editor/nlp/services/edits.py
+++ lexres/editor/nlp/services/edits.py
@@ -1,7 +1,5 @@
 from ...utils.nlp import SpanEdit
 """
     This script should contain only functions for edition proposals.
-    Functions will be called in order by the hook in editor/views.
     Functions should all share the same signature.
-    See preds.py for an example
 """
--- lexres/editor/nlp/services/preds.py
+++ lexres/editor/nlp/services/preds.py
@@ -1,5 +1,4 @@
 """
     This script should contain only functions for predicting the next word.
-    Functions will be called in order by the hook in editor/views.
     Functions should all share the same signature.
 """
