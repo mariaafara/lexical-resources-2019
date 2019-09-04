5a964f97228d19390ae2026da9eb97d850fcb0eb
Timothee Mickus <tmickus@justice.atilf.fr>
2019-09-04 14:19:10 +0200

    readability + example functions

--- lexres/editor/nlp/services/edits.py
+++ lexres/editor/nlp/services/edits.py
@@ -3,3 +3,8 @@ from ...utils.nlp import SpanEdit
     This script should contain only functions for edition proposals.
     Functions should all share the same signature.
 """
+def edit_bar_function(text=""):
+    """
+        Example function. To be removed
+    """
+    return [SpanEdit(beg_idx=1, end_idx=2, edit="test")]
--- lexres/editor/nlp/services/preds.py
+++ lexres/editor/nlp/services/preds.py
@@ -2,3 +2,8 @@
     This script should contain only functions for predicting the next word.
     Functions should all share the same signature.
 """
+def pred_foo_function(text=""):
+    """
+        Example function. To be removed
+    """
+    return ["a", "list", "of", "possible", "next", "words"]
